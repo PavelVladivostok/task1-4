63d9431740e214c9d8cdad38142d6fb96fd2c77a
PavelVladivostok <zihvav@gmail.com>
2024-10-03 00:28:44 +1000

    Add files via upload

---
+++ task1-4.py
@@ -0,0 +1,25 @@
+# 1 "1st program".
+
+print (9 ** 0.5*5)
+
+
+
+# "2nd program".
+
+print(9.98<9.99 or 1000!=1000.1)
+
+
+# "3rd program".
+
+
+
+print (2*2+2)
+print (2*(2+2))
+print (6==8)
+
+# «4-я программа».
+f=float='123,456'
+print(type(f))
+n=123.456
+print(type(n))
+print (int(n*10)%10)
